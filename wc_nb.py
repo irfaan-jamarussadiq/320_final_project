e1f746d06dd2ef38fe6e7c3b35846737a6ba613c
Irfaan <irfaanj9@gmail.com>
2021-05-13 19:44:56 -0400

    removed old notebok

--- wc_nb.py
+++ wc_nb.py
@@ -1,13 +1,30 @@
-import io
-import sys
-from nbformat import current
+from json import load
+from sys import argv
 
-filepath = sys.argv[1]
-with io.open(filepath, 'r', encoding='utf-8') as f:
-    nb = current.read(f, 'json')
+def loc(nb, option):
+    if option == 'code':
+        cells = load(open(nb))['cells']
+        f = lambda c : len(c['source'])
+        return sum(f(c) for c in cells)
+    else:
+        import io
+        from nbformat import current
 
-word_count = 0
-for cell in nb.worksheets[0].cells:
-    if cell.cell_type == "markdown":
-        word_count += len(cell['source'].replace('#', '').lstrip().split(' '))
-print(word_count)
+        with io.open(nb, 'r', encoding='utf-8') as f:
+            nb = current.read(f, 'json')
+
+        word_count = 0
+        for cell in nb.worksheets[0].cells:
+            if cell.cell_type == "markdown":
+                word_count += len(cell['source'].replace('#', '').lstrip().split(' '))
+        return word_count
+
+def run(ipynb_files):
+    x = sum(loc(nb, 'code') for nb in ipynb_files)
+    y = sum(loc(nb, 'markdown') for nb in ipynb_files)
+
+    print("Code:", x)
+    print("Markdown:", y)
+
+if __name__ == '__main__':
+    run(argv[1:])
